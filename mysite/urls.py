7bac0208751535144b3d8748fe44fa5b2d441eda
Divyanshi Agrawal <divyanshiagrawal07072000@gmail.com>
2021-04-10 14:57:10 +0530

    scramble_and_one_to_one_call

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,9 +6,11 @@ from . import views
 urlpatterns = [
 	path('scramble/', include('scramble.urls')),
     path('chat/', include('chat.urls')),
+    path('Call/', include('OneToOneCallChat.urls')),
     path('account/', include('UserHandle.urls')),
     path('admin/', admin.site.urls),
     path("home", views.home, name="home"),
     path("about", views.about, name="about"),
     path("contactus", views.contactus, name="contactus"),
+    
 ]
