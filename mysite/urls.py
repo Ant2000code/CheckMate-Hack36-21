8f9b82cdf1551609f3963728bb59aa2e3c05af9a
Divyanshi Agrawal <divyanshiagrawal07072000@gmail.com>
2021-04-11 04:06:13 +0530

    music_and_ui

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,15 +2,24 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('scramble/', include('scramble.urls')),
+	path('songs/', include('music_nation.urls')),
     path('chat/', include('chat.urls')),
     path('Call/', include('OneToOneCallChat.urls')),
     path('account/', include('UserHandle.urls')),
     path('admin/', admin.site.urls),
+    path('',views.intro,name="intro"),
     path("home", views.home, name="home"),
     path("about", views.about, name="about"),
+    path("faq", views.faq, name="faq"),
     path("contactus", views.contactus, name="contactus"),
+    path("tictactoe", views.tictactoe, name="tictactoe")
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -9,14 +9,20 @@ from django.conf import settings
 from django.core.mail import send_mail
 import json
 
-
 def about(request):
     return render(request, 'about.html')
-
 
 def contactus(request):
     return render(request, 'contactus.html')
 
-
 def home(request):
     return render(request, 'home.html')
+
+def tictactoe(request):
+    return render(request, 'tictactoe.html')
+
+def intro(request):
+    return render(request,'MainHome.html')
+
+def faq(request):
+    return render(request,'faq.html')
